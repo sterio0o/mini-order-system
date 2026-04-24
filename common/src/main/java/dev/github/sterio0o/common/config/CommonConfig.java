c5c0ebf7cba49f95461e28651a65519444c183bc
sterio0o <ilyanaumov203@gmail.com>
2026-04-24 10:32:19 +0400

    Added Spring Security to the common library

---
+++ common/src/main/java/dev/github/sterio0o/common/config/CommonConfig.java
@@ -0,0 +1,9 @@
+package dev.github.sterio0o.common.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("dev.github.sterio0o.common")
+public class CommonConfig {
+}
