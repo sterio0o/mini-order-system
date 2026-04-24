6a498179290822a8f01d6bfccffccb10786099e6
sterio0o <ilyanaumov203@gmail.com>
2026-04-24 20:45:29 +0400

    fix

--- user-service/src/main/java/dev/github/sterio0o/userservice/UserServiceApplication.java
+++ user-service/src/main/java/dev/github/sterio0o/userservice/UserServiceApplication.java
@@ -2,8 +2,10 @@ package dev.github.sterio0o.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("dev.github.sterio0o.common")
 public class UserServiceApplication {
 
     public static void main(String[] args) {
