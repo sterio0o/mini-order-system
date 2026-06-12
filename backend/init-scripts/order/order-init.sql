d9640b5de1873cce9e965fb8925c520d9f228a1c
sterio0o <ilyanaumov203@gmail.com>
2026-06-12 15:01:07 +0400

    Исправил конфликт портов

--- backend/init-scripts/order/order-init.sql
+++ backend/init-scripts/order/order-init.sql
@@ -10,7 +10,7 @@ CREATE TABLE orders (
     customer_email VARCHAR(64) NOT NULL,
     amount DECIMAL(10, 2) NOT NULL,
     status VARCHAR(32),
-    created_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL
 );
 
 CREATE TABLE order_items (
