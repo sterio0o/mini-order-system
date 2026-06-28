9bbb1e4dfdd5b0118cf8a3d0339543bc9d861ed0
sterio0o <ilyanaumov203@gmail.com>
2026-06-28 17:35:58 +0400

    Добавил генерацию QR code

--- backend/order-service/src/main/java/dev/github/sterio0o/orderservice/controller/OrderController.java
+++ backend/order-service/src/main/java/dev/github/sterio0o/orderservice/controller/OrderController.java
@@ -3,7 +3,11 @@ package dev.github.sterio0o.orderservice.controller;
 import dev.github.sterio0o.orderservice.model.dto.OrderRequestDto;
 import dev.github.sterio0o.orderservice.model.dto.OrderResponseDto;
 import dev.github.sterio0o.orderservice.service.OrderService;
+import dev.github.sterio0o.orderservice.service.QRCodeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -18,12 +22,29 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class OrderController {
     private final OrderService orderService;
+    private final QRCodeService qrCodeService;
 
     @GetMapping("/{id}")
     @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<OrderResponseDto> getOrderById(@PathVariable UUID id) {
         OrderResponseDto order = orderService.getOrderById(id);
         return ResponseEntity.ok(order);
+    }
+
+    @GetMapping("/qrcode/{id}")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    public ResponseEntity<byte[]> generateQRCode(@PathVariable UUID id) {
+        try {
+            byte[] qrCode = qrCodeService.generateQRCode(id);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.IMAGE_PNG);
+            headers.set(HttpHeaders.CONTENT_DISPOSITION, "inline");
+
+            return new ResponseEntity<>(qrCode, headers, HttpStatus.OK);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @PostMapping("/create")
